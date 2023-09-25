104485ee4190690fe4d37772a1e40164df8cedb2
user <user@MacBook-Pro-user.local>
2023-09-25 15:17:40 +0300

    Add count task functionality

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ function App(props) {
     <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
   ))
 
+  // counting tasks
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'
+  const headingText = `${taskList.length} ${tasksNoun} remaining`
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -30,7 +34,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
